f9edcd7a67b3800369cfc35f2402155497e59c8a
Momen-Alshouha <momenalshouha@outlook.com>
2022-03-23 10:53:06 +0300

    done

--- Javascript/21March/TASK2/main.js
+++ Javascript/21March/TASK2/main.js
@@ -32,12 +32,13 @@
 Sample numbers : 3, -7, 2 
 Output : The sign is - */
 /******* Start Your Code *********/
+
 function signOfproduct(num1 ,num2, num3){
     let multi=num1*num2*num3;
     if(multi<0){
         alert('-');
     }
-    
+
     else alert("+");
 }
 
