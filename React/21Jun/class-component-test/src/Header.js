fca2357e44acd83e15f923049c3ee2a16bea02ab
Momen-Alshouha <momenalshouha@outlook.com>
2022-06-21 16:15:39 +0300

    class-component practice

---
+++ React/21Jun/class-component-test/src/Header.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return (
+           <div className="container">
+             <div class="container jumbotron">
+                <h1 class="display-4">Hello, world!</h1>
+                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr class="my-4" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </div>
+
+            <div className="row">
+                <div class="card text-white bg-primary mb-3" style={{maxWidth: "23%",marginRight:"2%"}}>
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+
+                <div class="card text-white bg-danger mb-3" style={{maxWidth: "23%",marginRight:"2%"}}>
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+
+                <div class="card text-white bg-success mb-3" style={{maxWidth: "23%",marginRight:"2%"}}>
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+
+                <div class="card text-white bg-warning mb-3" style={{maxWidth: "23%",marginRight:"2%"}}>
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     </div>
+                     </div>
+            </div>
+
+
+            <div className="row">
+                <div class="card text-white bg-info mb-3" style={{maxWidth: "23%",marginRight:"2%"}}>
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+
+                <div class="card text-white bg-secondary mb-3" style={{maxWidth: "23%",marginRight:"2%"}}>
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+
+                <div class="card text-white bg-dark mb-3" style={{maxWidth: "23%",marginRight:"2%"}}>
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+
+                <div class="card text-white bg-primary mb-3" style={{maxWidth: "23%",marginRight:"2%"}}>
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Primary card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     </div>
+                     </div>
+            </div>
+           </div>
+
+           
+        )
+    }
+}
+
+
+export default Header
